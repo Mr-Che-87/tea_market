53a3ad36f92bf788843152b2f8ec724c59086e5f
Alexey Chechetkin <109634077+Mr-Che-87@users.noreply.github.com>
2024-11-29 11:45:00 +0300

    done

--- src/app/api/products/[id]/route.ts
+++ src/app/api/products/[id]/route.ts
@@ -1,17 +1,9 @@
 import { NextResponse } from "next/server";
-import { IProduct } from "@/types/IProduct";
-import { cache } from "react";
-
-const getProductById = cache(async (id: number) => { //кэшируем запрос к мок-БД
-  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/mock.json`);
-  if (!res.ok) throw new Error("Failed to fetch mock data");
-  const data = await res.json();
-  return data.products.find((p: IProduct) => p.id === id);
-});
+import mockData from "../../../../../public/mock.json";
 
 export async function GET(req: Request, { params }: { params: { id: string } }) {
   try {
-    const product = getProductById(Number(params.id));
+    const product = mockData.products.find((p) => p.id === Number(params.id));
 
     if (!product) {
       return NextResponse.json({ error: "Product not found" }, { status: 404 });
@@ -19,7 +11,6 @@ export async function GET(req: Request, { params }: { params: { id: string } }) 
 
     return NextResponse.json({ product });
   } catch (error) {
-    console.error("Error API", error);
     return NextResponse.error();
   }
 }
--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -1,20 +1,12 @@
 import { NextResponse } from "next/server";
-import { IProduct } from "@/types/IProduct";
-import { cache } from "react";
-
-const getProducts = cache(async () => {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/mock.json`);
-  if (!res.ok) throw new Error("Failed to fetch mock data");
-  const data = await res.json();
-  return data.products as IProduct[];
-});
+import mockData from "../../../../public/mock.json"; 
 
 export async function GET() {
   try {
-    const products = await getProducts();
+    const products = mockData.products; 
     return NextResponse.json({ products });
   } catch (error) {
-    console.error("Error API", error);
     return NextResponse.error();
   }
 }
+
